0e471dfb81a9a45c32f6223ee437e2aad3124bef
lamdaemon <i@lama.icu>
2025-09-09 12:00:58 -0700

    add initial macos support

--- src/sys_osx.c
+++ src/sys_osx.c
@@ -1,6 +1,7 @@
 #include "sys.h"
 
-#ifdef ZAKO_TARGET_POSIX
+#ifdef ZAKO_TARGET_APPLE
+#include <copyfile.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
@@ -38,51 +39,25 @@ file_handle_t zako_sys_file_opencopy(char* path, char* new, bool overwrite) {
         }
     }
 
-    /* Reference: https://man7.org/linux/man-pages/man2/copy_file_range.2.html#EXAMPLES */
+    /*
+        I know ther is copyfile function defined in "copyfile.h"
+        But if I dont use zako_syscall4, it will looks so weired to have a full set
+        of manual syscall functions.
 
-    int          fd_in, fd_out;
-    off_t        size, ret;
-    struct stat  stat;
-    
-    fd_in = open(path, O_RDONLY);
-    if (fd_in == -1) {
-        ConsoleWriteFAIL("Failed to open %s", path);
-        return -1;
-    }
-        
-    if (fstat(fd_in, &stat) == -1) {
-        ConsoleWriteFAIL("Failed to get file stats of %s (%s)", path, strerror(errno));
+        Also using manual syscall won't affect performance, so why not?
+        It looks cool, doesn't it?
+    */    
+    long rtn = zako_syscall4(SYS_copyfile, (long) path, (long) new, 0l, (long) COPYFILE_ALL);
 
-        close(fd_in);
-        return -1;
+    if (rtn != 0) {
+        ConsoleWriteFAIL("Failed to create a copy at %s (%li)", new, rtn);
     }
-        
-    size = stat.st_size;
-        
-    fd_out = open(new, O_CREAT | O_RDWR | O_TRUNC, 0644);
+
+    int fd_out = open(new, O_CREAT | O_RDWR | O_TRUNC, 0644);
     if (fd_out == -1) {
         ConsoleWriteFAIL("Failed to open %s", new);
         return -1;
     }
-        
-    do {
-        /* For some reason, copy_file_range is not defined in unistd...
-           For some reason, syscall is not defined in unistd either....
-
-           .... I am very confused ...
-           
-           In order to not copy from kernel to userspace and vise versa multiple times
-           we are going to manually do a syscall. Yay! */
-        ret = zako_syscall6(__NR_copy_file_range, fd_in, (long) NULL, fd_out, (long) NULL, size, 0);
-        if (ret == -1) {
-            ConsoleWriteFAIL("Failed copy %s to %s", path, new);
-            return -1;
-        }
-            
-        size -= ret;
-    } while (size > 0 && ret > 0);
-        
-    close(fd_in);
 
     return fd_out;
 }
